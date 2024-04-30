b74d3bf5cb137961663e56d1346985dbfaf3c20c
maab-fw <maabfw@proton.me>
2024-04-30 11:51:28 +0600

    newslatter section customized

--- src/components/NewsLetter/NewsLetter.jsx
+++ src/components/NewsLetter/NewsLetter.jsx
@@ -1,17 +1,15 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const NewsLetter = () => {
     return (
         <div>
-            <section className="bg-white dark:bg-gray-900">
-                <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-                    <div className="mx-auto max-w-screen-md sm:text-center">
-                        <h2 className="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl dark:text-white">
-                            Sign up htmlFor our newsletter
-                        </h2>
+            <section className="bg-base-100">
+                <div className="mx-auto ">
+                    <div className="mx-auto sm:text-center">
+                        <h2 className="mb-4 text-3xl tracking-tight text-center font-extrabold ">Subscribe for our newsletter</h2>
                         <p className="mx-auto mb-8 max-w-2xl font-light text-gray-500 md:mb-12 sm:text-xl dark:text-gray-400">
-                            Stay up to date with the roadmap progress, announcements and exclusive discounts feel free to sign up
-                            with your email.
+                            Stay up to date with new tourists spot by subscribing on our newsletter with your email.
                         </p>
                         <form action="#">
                             <div className="items-center mx-auto mb-3 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
@@ -34,7 +32,7 @@ const NewsLetter = () => {
                                         </svg>
                                     </div>
                                     <input
-                                        className="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                        className="block p-[13px] pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                         placeholder="Enter your email"
                                         type="email"
                                         id="email"
@@ -44,17 +42,17 @@ const NewsLetter = () => {
                                 <div>
                                     <button
                                         type="submit"
-                                        className="py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-primary-700 border-primary-600 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                                        className="btn md:rounded-none md:rounded-r-lg bg-info text-black border-0"
                                     >
                                         Subscribe
                                     </button>
                                 </div>
                             </div>
                             <div className="mx-auto max-w-screen-sm text-sm text-left text-gray-500 newsletter-form-footer dark:text-gray-300">
-                                We care about the protection of your data.{" "}
-                                <a href="#" className="font-medium text-primary-600 dark:text-primary-500 hover:underline">
+                                We care about the protection of your data.&#160;
+                                <Link className="font-medium text-primary-600 dark:text-primary-500 hover:underline">
                                     Read our Privacy Policy
-                                </a>
+                                </Link>
                                 .
                             </div>
                         </form>
